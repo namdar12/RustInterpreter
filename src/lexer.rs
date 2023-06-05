77af0b8a13637c974df7fc7a92fc4957e7cff403
namdar12 <namdar.mesri12@gmail.com>
2023-06-05 13:00:26 -0700

    fix lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -14,7 +14,7 @@ impl lexer {
         if self.read_position >= self.input.len() {
             self.ch = '\0'; 
         } else {
-            self.ch = self.input.chars().nth(self.read_position).unwrap_or('\0');
+            self.ch = self.input.as_bytes()[self.read_position] as char
         }
         self.position = self.read_position;
         self.read_position += 1;
@@ -44,102 +44,106 @@ impl lexer {
 
         }
 
-
+        self.read_char();
         return tok
    
-   
     }
 
 }
 
 
-
-
-
-
-
-pub fn test_next_token(){
-    let input = String::from("=+(){},;");
-
-    let expected = vec![
-        Token { 
-            kind: TokenKind::Assign, 
-            value: "=".to_string() 
-        },
-        Token {
-            kind: TokenKind::Plus,
-            value: "+".to_string(),
-        },
-        Token {
-            kind: TokenKind::Lparen,
-            value: "(".to_string(),
-        },
-        Token {
-            kind: TokenKind::Rparen,
-            value: ")".to_string(),
-        },
-        Token {
-            kind: TokenKind::Lbrace,
-            value: "{".to_string(),
-        },
-        Token {
-            kind: TokenKind::Rbrace,
-            value: "}".to_string(),
-        },
-        Token {
-            kind: TokenKind::Comma,
-            value: ",".to_string(),
-        },
-        Token {
-            kind: TokenKind::Semicolon,
-            value: ";".to_string(),
-        },
-        Token {
-            kind: TokenKind::EOF,
-            value: "".to_string(),
-        } ];
-
-    let mut l = new("test".to_string());
-
-    //ask about this
-    for (i,  tt) in expected.iter().enumerate() {
-
-        let tok = l.next_token();
-
-        assert_eq!(tok.kind, tt.kind, "Test[{}] - tokentype wrong. expected={}, got={}", i, tt.kind, tok.kind);
-        assert_eq!(tok.value, tt.value, "Test[{}] - literal wrong. expected={}, got={}", i, tt.value, tok.value);
-
-    }
-
-    
-}
 
 pub fn new(input: String) -> lexer {
-   let mut lex = lexer { input: "hello".to_string(), position:100, read_position: 10,ch: 'a'};
+   let mut lex = lexer {input: input.clone(), position: 0, read_position: 1, ch: input.as_bytes()[0] as char };
    lex.read_char();
    return lex
 }
 
 
 
-// pub fn add(x:u8, y:u8) -> u8 {
-//     x + y
-// }
+/*
+pub fn add(x:u8, y:u8) -> u8 {
+    x + y
+}
 
-// mod test {
-//     use super::add;
+mod test {
+    use super::add;
 
-//     #[test]
-//     fn add_works() {
-//         let (x, y) = (1,2 );
-//         let expected = 3;
-//         let output = add(x,y);
+    #[test]
+    fn add_works() {
+        let (x, y) = (1,2 );
+        let expected = 3;
+        let output = add(x,y);
 
-//         assert_eq!(expected, output);
-//     }
+        assert_eq!(expected, output);
+    }
 
-//     #[test]
-//     fn add_fails() {
-//         add(200, 100);
-//     }
-// }
+    #[test]
+    fn add_fails() {
+        add(200, 100);
+    }
+}
+*/
+
+mod test{
+    use super::lexer;
+    use crate::token::{Token, TokenKind};
+    use super::new;
+
+    #[test]
+    pub fn test_next_token(){
+        let input = String::from("=+(){},;");
+    
+        let expected = vec![
+            Token { 
+                kind: TokenKind::Assign, 
+                value: "=".to_string() 
+            },
+            Token {
+                kind: TokenKind::Plus,
+                value: "+".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lparen,
+                value: "(".to_string(),
+            },
+            Token {
+                kind: TokenKind::Rparen,
+                value: ")".to_string(),
+            },
+            Token {
+                kind: TokenKind::Lbrace,
+                value: "{".to_string(),
+            },
+            Token {
+                kind: TokenKind::Rbrace,
+                value: "}".to_string(),
+            },
+            Token {
+                kind: TokenKind::Comma,
+                value: ",".to_string(),
+            },
+            Token {
+                kind: TokenKind::Semicolon,
+                value: ";".to_string(),
+            },
+            Token {
+                kind: TokenKind::EOF,
+                value: "".to_string(),
+            } ];
+    
+        let mut l = new("test".to_string());
+    
+        //ask about this
+        for (i,  tt) in expected.iter().enumerate() {
+    
+            let tok = l.next_token();
+    
+            assert_eq!(tok.kind, tt.kind, "Test[{}] - tokentype wrong. expected={}, got={}", i, tt.kind, tok.kind);
+            assert_eq!(tok.value, tt.value, "Test[{}] - literal wrong. expected={}, got={}", i, tt.value, tok.value);
+    
+        }
+    
+        
+    }
+}
