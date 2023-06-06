2b25376b848f5fa82610443b6243c62da989b925
namdar12 <namdar.mesri12@gmail.com>
2023-06-05 18:16:47 -0700

    token stuff

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,6 @@
 use std::fmt;
+use std::collections::HashMap;
+
 
 #[derive(PartialEq)]
 #[derive(Debug)]
@@ -29,4 +31,30 @@ impl fmt::Display for TokenKind {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)  // or provide a custom representation
     }
+
 }
+
+impl TokenKind{
+    pub fn get_keywords(&self) -> HashMap<String, TokenKind> {
+        let initial_values= vec![
+            ("fn", TokenKind::Function),
+            ("let", TokenKind::Let),
+        ];
+
+        let hash = initial_values.into_iter().collect();
+        return hash
+        
+    }
+
+    pub fn look_up_ident(&self,ident: String) -> TokenKind {
+        let keywords = self.get_keywords();
+
+        match keywords.get(&ident) {
+            Some(token_type) =>  1000, //token_type.clone(),  // assuming TokenType implements Clone
+            None => 1000,
+        };
+
+        return TokenKind::Assign
+    }
+}
+
